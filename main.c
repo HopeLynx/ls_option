39f3dadf9c24dbfc8da7bc382b3a0c4376a1615e
HopeLynx <grigoriy.sizov@gmail.com>
2021-10-27 19:41:07 +0300

    name functions , several options , formatting add

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 //gcc main.c -o myLS
+// ./myLS -a .
 
 //Used for basic input/output stream
 #include <stdio.h>
@@ -7,67 +8,139 @@
 //For EXIT codes and error handling
 #include <errno.h>
 #include <stdlib.h>
+// new
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+
+#include <pwd.h>
+#include <grp.h>
 
 #define NULL ((void *)0)
 
-void _ls(const char *dir,int op_a,int op_l)
-{
-    //Here we will list the directory
-    struct dirent *d;
-    DIR *dh = opendir(dir);
-    if (!dh)
+const char *getUserName(int id) {
+    uid_t uid = id;
+    struct passwd *pw = getpwuid(uid);
+    if (pw)
     {
-        if (errno == ENOENT)
+        return pw->pw_name;
+    }
+    return "";
+}
+
+const char *getGroupName(int id) {
+    gid_t gid = id;
+    struct group *gr = getgrgid(gid);
+    if (gr)
+    {
+        return gr->gr_name;
+    }
+    return "";
+
+}
+
+void PrintFileInfo(int op_l,char* name){
+    struct stat s;
+    if( stat(name,&s) == 0 )
+    {
+        if(!op_l){
+            printf("%s ",name);
+        } else {
+            printf("%s %d %ld %ld %s %s \n",
+                          name,s.st_mode,s.st_nlink,s.st_size,
+                          getUserName(s.st_uid),getGroupName(s.st_gid));
+        }
+
+    } else {
+        perror("stat in print failed");
+    }
+}
+
+void _ls(const char *dir,int op_a,int op_l,int op_R)
+{
+    struct stat s;
+    if( stat(dir,&s) == 0 )
+    {
+        if( s.st_mode & S_IFDIR )
         {
-            //If the directory is not found
-            perror("Directory doesn't exist");
+            //it's a directory
+            //Here we will list the directory
+            struct dirent *d;
+            DIR *dh = opendir(dir);
+            if (!dh){
+                if (errno == ENOENT){
+                    //If the directory is not found
+                    perror("Directory doesn't exist");
+                } else {
+                    //If the directory is not readable then throw error and exit
+                    perror("Unable to read directory");
+                }
+                exit(EXIT_FAILURE);
+            }
+            //While the next entry is not readable we will print directory files
+            while ((d = readdir(dh)) != NULL){
+                //If hidden files are found we continue
+                if (!op_a && d->d_name[0] == '.')
+                    continue;
+                printf("%s  ", d->d_name);
+                if(op_l) printf("\n");
+            }
+            if(!op_l)
+                printf("\n");
+        }
+        else if( s.st_mode & S_IFREG )
+        {
+            //it's a file
+            printf("%s %d %ld %ld %s %s \n",
+                   dir,s.st_mode,s.st_nlink,s.st_size,
+                   getUserName(s.st_uid),getGroupName(s.st_gid));
         }
         else
         {
-            //If the directory is not readable then throw error and exit
-            perror("Unable to read directory");
+            //something else
+            perror("wtf is this path?");
         }
-        exit(EXIT_FAILURE);
     }
-    //While the next entry is not readable we will print directory files
-    while ((d = readdir(dh)) != NULL)
+    else
     {
-        //If hidden files are found we continue
-        if (!op_a && d->d_name[0] == '.')
-            continue;
-        printf("%s  ", d->d_name);
-        if(op_l) printf("\n");
+        //error
+        perror("stats unavailable");
     }
-    if(!op_l)
-    printf("\n");
+
+
+
+
+
+
+
+
+
 }
 
-//TODO R option , several options and 
+//TODO R option , several options and
 int main(int argc, const char *argv[])
 {
-    if (argc == 1)
-    {
-        _ls(".",0,0);
-    }
-    else if (argc == 2)
-    {
-        if (argv[1][0] == '-')
-        {
+    printf("%d\n",argc);
+    int op_a = 0, op_l = 0, op_R = 0, path_index = 0;
+    if (argc > 1) {
+        for (int i=1; i <= argc-1; i++)
             //Checking if option is passed
-            //Options supporting: a, l
-            int op_a = 0, op_l = 0;
-            char *p = (char*)(argv[1] + 1);
-            while(*p){
+            if (argv[i][0] == '-'){
+                //Options supporting: a, l, R
+                char *p = (char*)(argv[i] + 1);
                 if(*p == 'a') op_a = 1;
                 else if(*p == 'l') op_l = 1;
+                else if (*p == 'R') op_R = 1;
                 else{
                     perror("Option not available");
                     exit(EXIT_FAILURE);
                 }
-                p++;
+                //_ls(".",op_a,op_l);
+            } else {
+                path_index = i;
             }
-            _ls(".",op_a,op_l);
-        }
     }
+    path_index ? _ls(argv[path_index],op_a,op_l,op_R) : _ls(".",op_a,op_l,op_R) ;
+
     return 0;
 }
